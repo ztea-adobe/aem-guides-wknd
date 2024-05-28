4fd8be3048693abb6ff7335e317301e244e49a23
Zachary Tea <ztea@adobe.com>
2024-05-28 13:17:16 -0400

    Testing dvids call

--- ui.frontend/src/main/webpack/components/helloworld/ts/helloworld.js
+++ ui.frontend/src/main/webpack/components/helloworld/ts/helloworld.js
@@ -1,4 +1,22 @@
+import $ from 'jquery';
 let jsHelloWorld = () => {
     const jsString = "Hello World";
+    console.log($(document));
+    let SEARCH_API_ENDPOINT = "https://api.dvidshub.net/search?q=tank&api_key=key-664f9e8def833";
+    $.ajax({
+        url: SEARCH_API_ENDPOINT,
+        method: "GET",
+        success: function (data, status, xhr) {
+            console.log(data);
+            console.log(status);
+            console.log(xhr);
+        },
+        error: function (xhr, status, err) {
+            console.log('There was an error getting search results');
+
+        }, always: function (data, textStatus, errorThrown) {
+            console.log('always completed.');
+        }
+    });
 };
 export default jsHelloWorld();
